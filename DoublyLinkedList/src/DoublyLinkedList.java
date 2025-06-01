7611efac9cc3bf8bbaa3a09e2615a243ca4411a2
Sumanta Kumar Sahoo <sumant7654@gmail.com>
2025-06-02 00:18:55 +0530

    Delete Node from Doubly LinkedList

--- DoublyLinkedList/src/DoublyLinkedList.java
+++ DoublyLinkedList/src/DoublyLinkedList.java
@@ -9,13 +9,43 @@ public class DoublyLinkedList {
 
         insertNodeIntoDoubleLinkedList(25, 5);
         insertNodeIntoDoubleLinkedList(50, 2);
+        System.out.println("Traversal of DoublyLinkedList: ");
         traversalDoublyLinkedList(head);
-        System.out.println();
+        System.out.println("Reverse Traversal of DoublyLinkedList: ");
         reverseTraversalDoublyLinkedList(tail);
-        System.out.println();
+        System.out.println("Searching of Node Value in DoublyLinkedList: ");
         searchNodeValueInDoublyLinkedList(head, 50);
+        deleteNodeFromDoublyLinkedList(2);
+        System.out.println("Traversal of DoublyLinkedList After deleting A Node: ");
+        traversalDoublyLinkedList(head);
 
 
+
+    }
+
+    private static void deleteNodeFromDoublyLinkedList(int position) {
+        if(head == null) {
+            return;
+        }
+        if(position == 1) {
+            head = head.next;
+            head.prev = null;
+            size--;
+        }else if(position == size) {
+            tail = tail.prev;
+            tail.next = null;
+            size--;
+        }else{
+            int currentPosition = 1;
+            DoublyNode currentNode = head;
+            while(currentPosition < position - 1){
+                currentNode = currentNode.next;
+                currentPosition++;
+            }
+            currentNode.next = currentNode.next.next;
+            currentNode.next.prev = currentNode;
+            size--;
+        }
     }
 
     private static void searchNodeValueInDoublyLinkedList(DoublyNode head, int nodeValue) {
@@ -105,7 +135,7 @@ public class DoublyLinkedList {
 
     }
 
-    private static DoublyNode createDoublyLinkedList(int nodeValue) {
+    private static void createDoublyLinkedList(int nodeValue) {
         if(head == null){
             DoublyNode node = new DoublyNode();
             node.value = nodeValue;
@@ -113,6 +143,5 @@ public class DoublyLinkedList {
             head = tail = node;
             size = 1;
         }
-        return head;
     }
 }
