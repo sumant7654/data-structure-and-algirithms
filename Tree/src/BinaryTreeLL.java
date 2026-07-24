2739bcd918a5f118e9dccbf69887a89b11887be4
Sumanta Kumar Sahoo <sumant7654@gmail.com>
2026-07-24 16:09:45 +0530

    Binary Tree PreOrder Traversal

--- Tree/src/BinaryTreeLL.java
+++ Tree/src/BinaryTreeLL.java
@@ -3,4 +3,15 @@ public class BinaryTreeLL {
     public BinaryTreeLL(){
         this.root = null;
     }
+    /**
+    PreOrder Traversal -> First Visit Root Node then Left Sub tree and then right Sub Tree
+    */
+    void preOrder(BinaryNode node){
+        if(node == null){
+            return;
+        }
+        System.out.print(node.value+ " ");
+        preOrder(node.left);
+        preOrder(node.right);
+    }
 }
